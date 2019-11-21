5cfb3982bb03bc64acbff9f52325c6de21f17dc1
Mason Ashment <masonashment@gmail.com>
2019-11-20 21:47:42 -0500

    added methods to TaskContract to retrieve taskId and build a uri with specific taskid

--- app/src/main/java/com/gmail/masonashment/tasktimer/TaskContract.java
+++ app/src/main/java/com/gmail/masonashment/tasktimer/TaskContract.java
@@ -1,6 +1,11 @@
 package com.gmail.masonashment.tasktimer;
 
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
+
+import static com.gmail.masonashment.tasktimer.AppProvider.CONTENT_AUTHORITY;
+import static com.gmail.masonashment.tasktimer.AppProvider.CONTENT_AUTHORITY_URI;
 
 public class TaskContract {
 
@@ -17,4 +22,34 @@ public class TaskContract {
             //private to prevent instantiation
         }
     }
+
+    /**
+     * The URI to access the Tasks table
+     */
+    public static final Uri CONTENT_URI = Uri.withAppendedPath(CONTENT_AUTHORITY_URI, TABLE_NAME);
+
+    static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd." + CONTENT_AUTHORITY + "." + TABLE_NAME;
+    static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd." + CONTENT_AUTHORITY + "." + TABLE_NAME;
+
+    static Uri buildTaskUri(long taskId) {
+        return ContentUris.withAppendedId(CONTENT_URI, taskId);
+    }
+
+    static long getTaskId(Uri uri) {
+        return ContentUris.parseId(uri);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
